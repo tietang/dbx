3696ae953991cd36f23107f6b33b376f33d88c9c
nor236 <906033314@qq.com>
2026-01-14 14:07:26 +0800

    add sql log for getXX method

--- runner_querier.go
+++ runner_querier.go
@@ -326,22 +326,66 @@ func (r *Runner) QueryRowContext(ctx context.Context, sql string, params ...inte
 }
 
 func (r *Runner) GetInt32(sql string, params ...interface{}) (rv int32, err error) {
+	if r.LoggingEnabled() {
+		defer func(start time.Time) {
+			r.Logger().Log(&QueryStatus{
+				Query: sql,
+				Args:  params,
+				Err:   err,
+				Start: start,
+				End:   time.Now(),
+			})
+		}(time.Now())
+	}
 	row := r.sqlExecutor.QueryRow(sql, params...)
 	err = row.Scan(&rv)
 	return
 }
 
 func (r *Runner) GetInt64(sql string, params ...interface{}) (rv int64, err error) {
+	if r.LoggingEnabled() {
+		defer func(start time.Time) {
+			r.Logger().Log(&QueryStatus{
+				Query: sql,
+				Args:  params,
+				Err:   err,
+				Start: start,
+				End:   time.Now(),
+			})
+		}(time.Now())
+	}
 	row := r.sqlExecutor.QueryRow(sql, params...)
 	err = row.Scan(&rv)
 	return
 }
 func (r *Runner) GetString(sql string, params ...interface{}) (rv string, err error) {
+	if r.LoggingEnabled() {
+		defer func(start time.Time) {
+			r.Logger().Log(&QueryStatus{
+				Query: sql,
+				Args:  params,
+				Err:   err,
+				Start: start,
+				End:   time.Now(),
+			})
+		}(time.Now())
+	}
 	row := r.sqlExecutor.QueryRow(sql, params...)
 	err = row.Scan(&rv)
 	return
 }
 func (r *Runner) GetValue(out interface{}, sql string, params ...interface{}) (err error) {
+	if r.LoggingEnabled() {
+		defer func(start time.Time) {
+			r.Logger().Log(&QueryStatus{
+				Query: sql,
+				Args:  params,
+				Err:   err,
+				Start: start,
+				End:   time.Now(),
+			})
+		}(time.Now())
+	}
 	row := r.sqlExecutor.QueryRow(sql, params...)
 	err = row.Scan(out)
 	return
